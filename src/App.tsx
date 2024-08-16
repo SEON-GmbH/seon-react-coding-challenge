e626d529887799c179de5129e5eea1e2356e90cf
Timothy Hennessey <timothyhennessey@Timothys-MacBook-Pro.local>
2024-08-16 15:39:02 +0200

    chore: add alarms json and remove unnecessary files for challenge

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <p>Entry.</p>
+    <div className="app">
+      <h1>SEON Reacting Coding Challenge</h1>
+      <p>Alarms:</p>
     </div>
   );
 }
